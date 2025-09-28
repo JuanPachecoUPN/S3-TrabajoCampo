3072dc16ea3c5f2fc4d81c8471b38f2ff0b885f6
N0479037@upn.pe <jairo_155_145@hotmail.com>
2025-09-28 09:57:48 -0500

    subiendo programa 1y3

---
+++ Programa3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Ingrese el número de inicio: ");
+                int inicio = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el número de fin: ");
+                int fin = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\nNúmeros pares entre {inicio} y {fin}:");
+
+                for (int i = inicio; i <= fin; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
