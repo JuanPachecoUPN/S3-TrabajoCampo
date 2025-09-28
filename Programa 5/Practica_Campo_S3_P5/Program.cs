79e5dd2f6695d47c995856f6df29d4b480eea12a
Luis Machuca <n00420048@upn.pe>
2025-09-28 11:51:16 -0500

    PracticaCampoS3P5

---
+++ Programa 5/Practica_Campo_S3_P5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Campo_S3_P5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double promedio, nota1, nota2, nota3;
+
+            Console.Write("Ingrese primera nota: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese tercera nota: ");
+            nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("********************************");
+
+            promedio = Math.Round((nota1 + nota2 + nota3) / 3, 2);
+            Console.WriteLine($"Promedio: ({nota1} + {nota2} + {nota3}) / 3 = {promedio}");
+
+            if (promedio >= 17)
+            {
+                Console.WriteLine("\nTu promedio es EXCELENTE");
+            }
+            else
+            {
+                if (promedio >= 13 && promedio <= 16)
+                {
+                    Console.WriteLine("\nTu promedio es BUENO");
+                }
+                else
+                {
+                    if (promedio >= 10 && promedio <= 12)
+                    {
+                        Console.WriteLine("\nTu promedio es REGULAR");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nTu promedio es DEFICIENTE");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
